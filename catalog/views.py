3bbfe024fce19bc5e7c20810bcdde219746d8579
Andres Alvarez <andres15alvarez@gmail.com>
2024-09-30 21:24:04 -0400

    Fix filter

--- catalog/views.py
+++ catalog/views.py
@@ -46,8 +46,17 @@ class IllnessMedicineViewSet(ModelViewSet):
 class FrequentQuestionViewSet(ModelViewSet):
     permission_classes = [AllowAny]
     authentication_classes = [JWTAuthentication]
-    queryset = FrequentQuestion.objects.filter(deleted_at__isnull=True, is_visible=True)
+    queryset = FrequentQuestion.objects.filter(deleted_at__isnull=True)
     serializer_class = FrequentQuestionSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_authenticated:
+            if not self.request.user.is_admin:
+                queryset = queryset.filter(is_visible=True)
+        else:
+            queryset = queryset.filter(is_visible=True)
+        return queryset
 
     def perform_destroy(self, instance):
         instance.deleted_at = now()
