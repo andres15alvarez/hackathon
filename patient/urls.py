7fe0c45a03954af18e9d200161da1fe7251f00ec
Andres Alvarez <andres15alvarez@gmail.com>
2024-10-01 21:16:47 -0400

    Register endpoint

--- patient/urls.py
+++ patient/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from patient.views import PatientListAPIView
+from patient.views import PatientListAPIView, PatientDetailAPIView
 
 
 app_name = "patient"
 
 urlpatterns = [
     path("", PatientListAPIView.as_view(), name="patient_list"),
+    path("<uuid:pk>", PatientDetailAPIView.as_view(), name="patient_detail"),
 ]
